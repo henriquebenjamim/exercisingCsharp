59f9b51bc4da95b2a99c54f587d1398bd080a069
henriquebenjamim <henriiqqq@gmail.com>
2022-04-05 12:28:09 -0300

    adding ex 016

--- w3resource/ex016/ex016/Program.cs
+++ w3resource/ex016/ex016/Program.cs
@@ -6,7 +6,23 @@ namespace ex016
     {
         static void Main(string[] args)
         {
+            //string s = Console.ReadLine();
+            //Console.WriteLine(s[0]);
+            //Console.WriteLine(s[s.Length -1]);
+            //s += 'a';
+            //Console.WriteLine(s);
+
+            //string nome = Console.ReadLine();
+            //Console.Write($"{nome.Substring(nome.Length - 1)}{nome.Substring(1, nome.Length - 2)}{nome.Substring(0, 1)}");
             
+            string s = Console.ReadLine();
+            Console.WriteLine(s);  
+            Console.WriteLine(switchingPosition(s));
+        }
+
+        public static string switchingPosition(string s)
+        {
+            return s.Length > 1 ? s.Substring(s.Length - 1) + s.Substring(1, s.Length - 2) + s.Substring(0, 1) : s;
         }
     }
 }
