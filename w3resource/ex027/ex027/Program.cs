5275dcac2fb616fee8c5d4f8b05f509d5ba5e4a3
henriquebenjamim <henriiqqq@gmail.com>
2022-04-06 22:24:49 -0300

    Adding ex027

---
+++ w3resource/ex027/ex027/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex027
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //27.Write a C# program and compute the sum of the digits of an integer.
+            //Sample Output:
+            //Input a number(integer): 12
+            //Sum of the digits of the said integer: 3
+
+            Console.Write("Input a number(integer): ");
+            string number = Console.ReadLine();
+            number.ToCharArray();
+            int counting = 0;
+            foreach(char value in number)
+            {
+                int realNumber = value - '0';
+                counting += realNumber;
+                
+            }
+            Console.WriteLine(counting);
+
+        }
+    }
+}
