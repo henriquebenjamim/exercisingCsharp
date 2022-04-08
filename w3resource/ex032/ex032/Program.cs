0f872498d0f3f44b3b1d91f8b83e667fe935eb45
henriquebenjamim <henriiqqq@gmail.com>
2022-04-08 12:47:02 -0300

    Adding ex032 basic

---
+++ w3resource/ex032/ex032/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex032
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //32.Write a C# program to create a new string of four copies, taking last four characters from a given string.
+            //If the length of the given string is less than 4 return the original one.
+            //Sample Output:
+            //Input a string : The quick brown fox jumps over the lazy dog.
+            //dog.dog.dog.dog.
+
+            Console.Write("Input a string: ");
+            string phrase = Console.ReadLine();
+            string[] phraseArr = phrase.Split(' ');
+            
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"{phraseArr[phraseArr.Length -1]}");
+            }
+
+        }
+    }
+}
