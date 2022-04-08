a0f760729cd3053171d55836af43535aecf05a06
henriquebenjamim <henriiqqq@gmail.com>
2022-04-08 01:15:19 -0300

    Adding ex 029 basic

---
+++ w3resource/ex029/ex029/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace ex029
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // C:\exercising\w3resource\ex029\textFile.txt
+            string fileName = "C:/exercising/w3resource/ex029/textFile.txt";
+            FileInfo fi = new FileInfo(fileName);
+            string justFileName = fi.Name;
+            long size = fi.Length;
+            Console.WriteLine($"File Name: {justFileName}");
+            Console.WriteLine($"File size in Bytes: {size}");
+        }
+    }
+}
