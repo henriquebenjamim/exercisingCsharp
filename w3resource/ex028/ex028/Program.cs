fd1ea410ec0f30671fdb7b4a1343d9f277da0360
henriquebenjamim <henriiqqq@gmail.com>
2022-04-08 00:20:47 -0300

    Resolving ex028 basic exercises

---
+++ w3resource/ex028/ex028/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex028
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //28.Write a C# program to reverse the words of a sentence.
+            //Sample Output:
+            //Original String: Display the pattern like pyramid using the alphabet.
+            //Reverse String: alphabet.the using pyramid like pattern the Display
+
+            Console.WriteLine("Original String:");
+            string originalString = Console.ReadLine();
+            string[] ori = originalString.Split(' ');
+            string reverseString;
+            for (int i = ori.Length -1; i >= 0; i--)
+            {
+                Console.WriteLine(ori[i]);
+
+            }
+        }
+
+        public static string reverseString(string original)
+        {
+            return "ok";
+        }
+    }
+}
