ec50bdf55fad6b5c58749415ef79db16688c3336
henriquebenjamim <henriiqqq@gmail.com>
2022-04-08 12:55:35 -0300

    add ex033 resolved

---
+++ w3resource/ex033/ex033/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex033
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //33.Write a C# program to check if a given positive number is a multiple of 3 or a multiple of 7.
+            //Sample Output:
+            //Input first integer: 15       
+            //True
+
+            Console.Write("Input first integer: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if(number > 0)
+            {
+                Console.WriteLine((number % 3 == 0 || number % 7 == 0));
+            }
+            
+        }
+    }
+}
