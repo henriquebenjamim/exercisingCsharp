04d5d120ac4f9ba3a24890a45088588256ff312f
henriquebenjamim <henriiqqq@gmail.com>
2022-04-07 23:46:58 -0300

    Resolving ex002

--- w3resource-basicAlgorithm/ex002/ex002/Program.cs
+++ w3resource-basicAlgorithm/ex002/ex002/Program.cs
@@ -6,7 +6,22 @@ namespace ex002
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //2.Write a C# Sharp program to get the absolute difference between n and 51.
+            //If n is greater than 51 return triple the absolute difference.
+
+            //Sample Input:               Expected Output:
+            //53                          6
+            //30                          21
+            //51                          0
+
+            Console.WriteLine(absoluteDifference(53));
+            Console.WriteLine(absoluteDifference(30));
+            Console.WriteLine(absoluteDifference(51));
+        }
+
+        public static int absoluteDifference(int number)
+        {
+            return number > 51 ? Math.Abs(number - 51) * 3 : Math.Abs(number - 51);
         }
     }
 }
