d5ff3ea0209d6661a45bafe78387d97c8b46e866
henriquebenjamim <henriiqqq@gmail.com>
2022-04-08 02:19:38 -0300

    Adding 29, 30, 31 basic exercises

---
+++ w3resource/ex030/ex030/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex030
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //30.Write a C# program to convert a hexadecimal number to decimal number. 
+            //Sample Output:
+            //Hexadecimal number: 4B0
+            //Convert to -
+            //Decimal number: 1200
+
+            Console.Write("Type hexadecimal value: ");
+            string hexaDecimalValue = Console.ReadLine();
+
+            int decimalValue = Convert.ToInt32(hexaDecimalValue, 16);
+            Console.WriteLine(decimalValue);
+        }
+    }
+}
---
+++ w3resource/ex031/ex031/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ex031
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //31.Write a C# program to multiply corresponding elements of two arrays of integers.
+            //Sample Output:
+            //Array1: [1, 3, -5, 4]
+            //Array2: [1, 4, -5, -2]
+            //Multiply corresponding elements of two arrays:
+            //1 12 25 - 8
+
+            Console.Write("Type 4 values of first array: ");
+            string firstArrayString = Console.ReadLine();
+            Console.Write("Type 4 values of second array: ");
+            string secondArrayString = Console.ReadLine();
+
+            string[] firstArr = firstArrayString.Split(' ');
+            string[] secondArr = secondArrayString.Split(' ');
+            
+            int[] multiArr = new int[4];
+            int numberOne, numberTwo;
+            for(int i = 0; i < 4; i++)
+            {
+                numberOne = Int16.Parse(firstArr[i]);
+                numberTwo = Int16.Parse(secondArr[i]);
+                multiArr[i] = numberOne * numberTwo;
+            }
+            foreach(int number in multiArr)
+            {
+                Console.Write($"{number} ");
+            }
+        }
+    }
+}
