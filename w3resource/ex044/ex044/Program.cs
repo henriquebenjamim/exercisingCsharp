9be2abc961fdb3129fe30278078ab36880d5a254
henriquebenjamim <henriiqqq@gmail.com>
2022-04-09 16:15:00 -0300

    Adding ex044 basic categorie

---
+++ w3resource/ex044/ex044/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex044
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //44.Write a C# program to create a new string of every other character (odd position) from the first position of a given string.
+            //Test Data:
+            //Input a string : w3resource
+            //Sample Output:
+            //wrsuc
+
+            Console.Write("Input a string: ");
+            string word = Console.ReadLine();
+            for(int i=0; i <= word.Length - 1; i++)
+            {
+                if(i % 2 != 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.Write(word[i]);
+                }
+            }
+
+        }
+    }
+}
