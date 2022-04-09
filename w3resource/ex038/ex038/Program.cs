f098b7bb24ae5e33c5ed6712e7d8eebe4826c351
henriquebenjamim <henriiqqq@gmail.com>
2022-04-09 13:12:51 -0300

    Adding ex038 basic categorie

---
+++ w3resource/ex038/ex038/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex038
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //38.Write a C# program to get a new string of two characters from a given string. The first and second character of the given string must be "P" and "H", so PHP will be "PH".
+            //Test Data: PHP
+            //Sample Output: PH
+
+            Console.Write("Type a string phrase: ");
+            string phrase = Console.ReadLine();
+
+            if(phrase.Substring(0, 2) == "PH")
+            {
+                Console.WriteLine(phrase.Substring(0, 2));
+            }
+
+        }
+    }
+}
