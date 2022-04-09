99dacdc7ef7b45fcaf855837b13f847bddb6315e
henriquebenjamim <henriiqqq@gmail.com>
2022-04-09 13:28:23 -0300

    Adding ex039 basic categorie

---
+++ w3resource/ex039/ex039/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace ex039
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //39.Write a C# program to find the largest and lowest values from three integer values.
+            //Test Data:
+            //Input first integer: 15         
+            //Input second integer: 25          
+            //Input third integer: 30
+            //Sample Output
+            //Largest of three: 30
+            //Lowest of three: 15
+
+            Console.Write("Input first integer: ");
+            int firstInteger = int.Parse(Console.ReadLine());
+            Console.Write("Input first integer: ");
+            int secondInteger = int.Parse(Console.ReadLine());
+            Console.Write("Input first integer: ");
+            int thirdInteger = int.Parse(Console.ReadLine());
+
+            int[] numberArr = { firstInteger, secondInteger, thirdInteger };
+            
+            Console.WriteLine($"Largest of three: {numberArr.Max()}");
+            Console.WriteLine($"Lowest of three: {numberArr.Min()}");
+        }
+    }
+}
