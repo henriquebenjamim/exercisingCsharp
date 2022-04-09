d10e38d8efeabc3e8e6c5f93727f6f58ed91fa42
henriquebenjamim <henriiqqq@gmail.com>
2022-04-09 15:58:33 -0300

    adding ex042 basic categorie

---
+++ w3resource/ex042/ex042/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ex042
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //42.Write a C# program to create a new string where the first 4 characters will be in lower case. If the string is less than 4 characters then make the whole string in upper case.
+            //Test Data:
+            //Input a string: w3r
+            //Sample Output: W3R
+
+            Console.Write("Input a string: ");
+            string wordString = Console.ReadLine();
+            Console.WriteLine(differentStr(wordString));
+
+            // if > 4char || string.toLower(substring(0, 3) : string.toUpper()
+            
+        }
+
+        public static string differentStr(string word)
+        {
+            if (word.Length > 4)
+            {
+                string cuttingWord = word.Substring(0, 3);
+                return cuttingWord.ToLower() + word.Substring(3);
+            }
+            else
+            {
+                return word.ToUpper();
+            }
+                
+        }
+    }
+}
