98c403a2c40e3c6f936d81662e682df7909ca46e
henriquebenjamim <henriiqqq@gmail.com>
2022-04-09 12:42:17 -0300

    Adding ex035 basic categorie

---
+++ w3resource/ex035/ex035/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //35.Write a C# program to check two given numbers where one is less than 100 and other is greater than 200.
+            //Sample Output:
+            //Input a first number(< 100): 75
+            //Input a second number(> 100): 250
+            //True
+
+            Console.Write("Input a first number(< 100): ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Input a second number(> 100): ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((firstNumber < 100 && secondNumber > 200) || firstNumber > 200 && secondNumber < 100);
+        }
+    }
+}
