bbfab11b0bfe13de494b70cbe0d5443c9b60a00e
henriquebenjamim <henriiqqq@gmail.com>
2022-04-09 12:46:03 -0300

    Adding ex036 basic categorie

---
+++ w3resource/ex036/ex036/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex036
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //36.Write a C# program to check if an integer (from the two given integers) is in the range -10 to 10.
+            //Sample Output:
+            //Input a first number: -5
+            //Input a second number: 8
+            //True
+
+            Console.Write("Input a first number: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Input a second number: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((firstNumber >= -10 && firstNumber <= 10) || secondNumber >= -10 && secondNumber <= 10);
+        }
+    }
+}
