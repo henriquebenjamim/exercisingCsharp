806c8e8273360b03855fb9afbb8f58b922e218a5
henriquebenjamim <henriiqqq@gmail.com>
2022-04-09 12:34:02 -0300

    Adding ex034 - basic

---
+++ w3resource/ex034/ex034/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex034
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //34.Write a C# program to check if a string starts with a specified word.
+            //Note: Suppose the sentence starts with "Hello"
+            //Sample Data: string1 = "Hello how are you?"
+            //Result: Hello.
+            //Sample Output:
+            //Input a string : Hello how are you?
+            //True
+
+            Console.Write("Input a string: ");
+            string word = Console.ReadLine();
+            string[] wordsArr = word.Split(' ');
+            Console.WriteLine(wordsArr[0] == "Hello");
+            
+        }
+    }
+}
