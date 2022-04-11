77071535ed18eb105213cdc8d68bbe15fd63f781
henriquebenjamim <henriiqqq@gmail.com>
2022-04-10 21:31:16 -0300

    Adding ex054 basic category

---
+++ w3resource/ex054/ex054/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ex054
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //54.Write a C# program to get the century from a year.
+            Console.WriteLine(century(1900));
+            Console.WriteLine(century(1560));
+
+        }
+
+        public static int century(int year)
+        {
+            return year / 100 + ((year % 100 == 0) ? 0 : 1);
+        }
+    }
+}
