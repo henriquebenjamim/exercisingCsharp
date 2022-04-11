39ce4bb3bd651abf6d01cdc5fd37d53dfbe2fd20
henriquebenjamim <henriiqqq@gmail.com>
2022-04-11 20:57:25 -0300

    Adding ex 061 basic category

---
+++ w3resource/ex061/ex061/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ex061
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //61.Write a C# program to sort the integers in ascending order without moving the number -5.
+            int[] arr = sortingNumbers(new int[] { -5, 236, 120, 70, -5, -5, 698, 280 });
+
+            foreach(var value in arr)
+            {
+                Console.WriteLine(value.ToString());
+            }
+        }
+
+        public static int[] sortingNumbers(int[] arr)
+        {
+            int[] num = arr.Where(x => x != -5).OrderBy(x => x).ToArray();
+            int counting = 0;
+
+            return arr.Select(x => x >= 0 ? num[counting++] : -5).ToArray();
+        }
+
+        
+    }
+}
