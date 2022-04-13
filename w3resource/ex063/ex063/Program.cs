f7a625e5048cf9900a4db83646f524d48a44257b
henriquebenjamim <henriiqqq@gmail.com>
2022-04-12 21:58:55 -0300

    Adding ex063 basic category

---
+++ w3resource/ex063/ex063/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+
+namespace ex063
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //63.Write a C# program to check if a given number present in an array of numbers
+
+            int[] numbersOne = { 1, 2, 3, 5, 6, 9, 10 };
+            int[] numberTwo = { 2, 4, 6, 8, 10 };
+            int[] numberThree = { 1, 3, 5, 7, 9 };
+
+            Console.Write("What number? ");
+            int number = int.Parse(Console.ReadLine());
+
+            foreach(int value in numbersOne)
+            {
+                if(value == number)
+                {
+                    Console.WriteLine("It's here.");
+                }
+            }
+            Console.WriteLine(checkNumber(numberTwo, number));
+        }
+
+        public static string checkNumber(int[] arr, int number)
+        {
+            int indexOfNumbers = Array.IndexOf(arr, number);
+            if(indexOfNumbers > -1)
+            {
+                return $"{number} is PRESENT on {indexOfNumbers} index.";
+            }
+            else
+            {
+                return $"{number} isn't PRESENT";
+            }
+        }
+    }
+}
