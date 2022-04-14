6941350db7db813245fcecc26cebd648f70be48d
henriquebenjamim <henriiqqq@gmail.com>
2022-04-14 19:23:30 -0300

    Resolve ex069 basic category

--- w3resource/ex069/ex069/Program.cs
+++ w3resource/ex069/ex069/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex069
 {
@@ -7,6 +8,38 @@ namespace ex069
         static void Main(string[] args)
         {
             //69.Write a C# Sharp program to check if a given string contains only lowercase or uppercase characters.
+
+            string nameUpper = "HENRIQUE";
+            string nameLower = "jujubas";
+            Console.WriteLine(nameUpper.All(x => char.IsUpper(x)));
+            Console.WriteLine(nameLower.All(x => char.IsUpper(x)));
+
+            Console.WriteLine(nameUpper.All(x => char.IsLower(x)));
+            Console.WriteLine(nameLower.All(x => char.IsLower(x)));
+            Console.WriteLine(checkUpperOrLower("Almondegas"));
+            Console.WriteLine(checkUpperOrLower("almofadas"));
+        }
+
+        public static string checkUpperOrLower(string str)
+        {
+            bool strUpper = str.All(x => char.IsUpper(x));
+            bool strLower = str.All(x => char.IsLower(x));
+            string answer = "";
+
+            if(strUpper)
+            {
+                answer = "Upper";
+            }
+            else if(strLower)
+            {
+                answer = "Lower";
+            }
+            else
+            {
+                answer = "None";
+            }
+            return answer;
+
         }
     }
 }
