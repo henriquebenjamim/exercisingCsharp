f66621560ff216e249fe23e9dc5f1c3a9f0f0752
henriquebenjamim <henriiqqq@gmail.com>
2022-04-14 00:14:59 -0300

    Adding ex065 basic category

---
+++ w3resource/ex065/ex065/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex065
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //65.Write a C# Sharp program to multiply all of elements of a given array of numbers by the array length.
+
+
+            int[] numbers = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            int lenArr = numbers.Length;
+            int[] newArr = new int[numbers.Length];
+
+            for(int i = 0; i <= lenArr - 1; i++)
+            {   
+                newArr[i] = numbers[i] * (int)lenArr;
+            }
+            foreach(int value in newArr)
+            {
+                Console.Write($"{value} ");
+            }
+
+        }
+
+    }
+}
