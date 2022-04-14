664275603b49116d5a3e65dadddc110761e27cf7
henriquebenjamim <henriiqqq@gmail.com>
2022-04-14 18:59:55 -0300

    resolving ex068 basic category

--- w3resource/ex068/ex068/Program.cs
+++ w3resource/ex068/ex068/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex068
 {
@@ -7,6 +8,21 @@ namespace ex068
         static void Main(string[] args)
         {
             //68.Write a C# Sharp program to count a specified character (both cases) in a given string.
+
+            string name = "hEnrique";
+            int countingCharacterUpper = name.Count(x => x == 'E');
+            int countingCharacterLower = name.Count(x => x == 'e');
+            Console.WriteLine(countingCharacterUpper);
+            Console.WriteLine(countingCharacterLower);
+            Console.WriteLine(countingCharacterUpper + countingCharacterLower);
+            Console.WriteLine(countingCharacters("Henrique EEA", 'E', 'e'));
+        }
+
+        public static int countingCharacters(string str, char characterUpper, char characterLower)
+        {
+            int countingCharacterUpper = str.Count(x => x == 'E');
+            int countingCharacterLower = str.Count(x => x == 'e');
+            return countingCharacterLower + countingCharacterUpper;
         }
     }
 }
