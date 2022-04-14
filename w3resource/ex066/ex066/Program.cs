4fa57a209ff9b6fc0aa994e58e393784f1cdf892
henriquebenjamim <henriiqqq@gmail.com>
2022-04-14 00:46:54 -0300

    Adding ex066 basic category

---
+++ w3resource/ex066/ex066/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ex066
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# Sharp program to find the minimum value from two given two numbers, represented as string.
+
+            Console.Write("Write a number: ");
+            string number = Console.ReadLine();
+            if(number.Length > 2)
+            {
+                Console.WriteLine("Not found. 2 numbers needed.");
+            }
+            else
+            {
+                if (number.Length == 2)
+                {
+                    char[] converting = number.ToCharArray();
+                    if((int)converting[0] > (int)converting[1])
+                    {
+                        Console.WriteLine(converting[0]);
+                    }
+                    else if((int)converting[0] < (int)converting[1])
+                    {
+                        Console.WriteLine(converting[1]);
+                    }
+
+                }
+            }
+        }
+    }
+}
