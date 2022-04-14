9af0ca2c2d875685cac307c368d9d3ec2b6ab886
henriquebenjamim <henriiqqq@gmail.com>
2022-04-13 23:04:50 -0300

    Adding ex 064 basic category

---
+++ w3resource/ex064/ex064/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ex064
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //64.Write a C# Sharp program to get the file name (including extension) from a given path
+
+            string path = @"C:\exercising\w3resource\ex064\henrique.txt";
+            File.Create(path);
+            Console.WriteLine("Ok");
+
+            Console.WriteLine(path.Split('/').Last());
+
+        }
+    }
+}
