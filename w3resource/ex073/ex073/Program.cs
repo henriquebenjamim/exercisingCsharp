cdcc5c7f8c49d248728bdbf28c0fb960ba93fb20
henriquebenjamim <henriiqqq@gmail.com>
2022-04-15 11:36:29 -0300

    ex073 basic category resolved

--- w3resource/ex073/ex073/Program.cs
+++ w3resource/ex073/ex073/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex073
 {
@@ -14,6 +15,31 @@ namespace ex073
             //Convert the letters of the said string into alphabetical order: aacijprstv
             //Original string: python
             //Convert the letters of the said string into alphabetical order: hnopty
+
+            Console.Write("Original string: ");
+            string word = Console.ReadLine();
+            char[] wordChar = word.ToCharArray();
+            Array.Sort(wordChar);
+
+            if (wordChar.All(x => char.IsUpper(x)))
+            {
+                foreach (char v in wordChar)
+                {
+                    Console.WriteLine(v);
+                }
+            }
+            else if(wordChar.All(x => char.IsLower(x)))
+            {
+                foreach (char v in wordChar)
+                {
+                    Console.WriteLine(v);
+                }
+            }
+            else
+            {
+                Console.WriteLine("This string need to be only in lowercase/upper");
+            }
+            
         }
     }
 }
