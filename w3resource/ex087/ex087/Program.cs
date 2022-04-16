0fdeecd52866045e17b755a76527a293fc4c6eff
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 16:18:17 -0300

    ex087 basic category resolved

--- w3resource/ex087/ex087/Program.cs
+++ w3resource/ex087/ex087/Program.cs
@@ -12,6 +12,18 @@ namespace ex087
             //Reverse value: True
             //Original value: True
             //Reverse value: False
+            bool status = true;
+            bool status2 = false;
+            bool status3 = !false;
+            Console.WriteLine(reversing(status));
+            Console.WriteLine(reversing(status2));
+            Console.WriteLine(reversing(status3));
+        }
+
+
+        public static bool reversing(bool status)
+        {
+            return !status;
         }
     }
 }
