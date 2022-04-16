05b3cc278396491e991f1ba60cb6121e841799a4
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 16:49:48 -0300

    ex091 basic category resolved

--- w3resource/ex091/ex091/Program.cs
+++ w3resource/ex091/ex091/Program.cs
@@ -11,6 +11,16 @@ namespace ex091
             //Original array elements:
             //25 Anna False 4 / 24 / 2021 11:43:11 AM - 112 - 34.67
             //After removing all the values except integer values from the said array of mixed values: 25 - 112
+
+            object[] arr = { 25, "Anna", false, DateTime.Now, 112, 34.67 };
+
+            foreach(object value in arr)
+            {
+                if(value is int)
+                {
+                    Console.Write($"{value} ");
+                }
+            }
         }
     }
 }
