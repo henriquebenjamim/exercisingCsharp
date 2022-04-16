54aee3ccaca59603b209b8d8cfc6d0fa5412ae3a
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 16:28:08 -0300

    ex089 basic category resolved

--- w3resource/ex089/ex089/Program.cs
+++ w3resource/ex089/ex089/Program.cs
@@ -17,6 +17,38 @@ namespace ex089
             //Original Array elements:
             //Number of positive numbers: 0
             //Number of negative numbers: 0
+            int[] firstArr = { 10, -11, 12, -13, 14, -18, 19, -20 };
+            int[] secondArr = { -4, -3, -2, 0, 3, 5, 6, 2, 6 };
+            int[] emptyArr = { };
+
+            Console.WriteLine(amountPositiveNegative(firstArr));
+            Console.WriteLine();
+
+            Console.WriteLine(amountPositiveNegative(secondArr));
+            Console.WriteLine();
+
+            Console.WriteLine(amountPositiveNegative(emptyArr));
+            Console.WriteLine();
+        }
+
+        public static string amountPositiveNegative(int[] arrNumbers)
+        {
+            int negativeNumbers = 0;
+            int positiveNumbers = 0;
+
+            foreach (int number in arrNumbers)
+            {
+                if (number < 0)
+                {
+                    negativeNumbers += 1;
+                }
+                else
+                {
+                    positiveNumbers += 1;
+                }
+            }
+            return $"Number of positive numbers: {positiveNumbers}\n" +
+                    $"Number of negative numbers: {negativeNumbers}";
         }
     }
 }
