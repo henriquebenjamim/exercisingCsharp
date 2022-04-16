2436fa8c0cae9b04bcad9d7602e0eb4ea7fb64b9
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 00:24:07 -0300

    ex078 basic category resolved

--- w3resource/ex078/ex078/Program.cs
+++ w3resource/ex078/ex078/Program.cs
@@ -10,6 +10,23 @@ namespace ex078
             //Sample Output:
             //Sum of squares of elements of the said array: 14
             //Sum of squares of elements of the said array: 29
+
+            int[] numbers = { 1, 2, 3 };
+            int[] numbersTwo = { 2, 3, 4 };
+
+            Console.WriteLine(squareElementsArray(numbers));
+            Console.WriteLine(squareElementsArray(numbersTwo));
+        }
+
+        public static string squareElementsArray(int[] arr)
+        {
+            int amount = 0;
+            foreach(int value in arr)
+            {
+                amount += value * value;
+            }
+
+            return $"Sum of squares of elements of the said array: {amount}";
         }
     }
 }
