579bc03050e7e4de9e733b0dbabd1f10ced2bd56
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 16:22:41 -0300

    ex088 basic category resolved

--- w3resource/ex088/ex088/Program.cs
+++ w3resource/ex088/ex088/Program.cs
@@ -11,8 +11,17 @@ namespace ex088
             //In geometry, a polygon is a plane figure that is described by a finite number of straight line segments connected to form a closed polygonal chain or polygonal circuit.
             //The solid plane region, the bounding circuit, or the two together, may be called a Polygon.
             //Sample Output:
-            //Input number of straight lines of the polygon:
+            //Input number of straight lines of the polygon: 0
             //Sum of the interior angles(in degrees) of the said polygon: -360
+
+            Console.WriteLine("Input a number of straight lines of the polygon: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Sum of the interior angles(n degrees): {calculating(number)}");
+        }
+
+        public static int calculating(int number)
+        {
+            return 180 * (number - 2);
         }
     }
 }
