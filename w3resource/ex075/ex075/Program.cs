71937d2301c0e8fa5d64f5092458adad0fc8af87
henriquebenjamim <henriiqqq@gmail.com>
2022-04-15 23:40:09 -0300

    ex075 basic category resolved

--- w3resource/ex075/ex075/Program.cs
+++ w3resource/ex075/ex075/Program.cs
@@ -12,6 +12,25 @@ namespace ex075
             //2nd odd number: 3
             //4th odd number: 7
             //100th odd number: 199
+
+            Console.Write("Give me a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if(number % 2 == 0)
+            {
+                Console.WriteLine($"{number}th odd number: {number*2 - 1}");
+            }
+            else
+            {
+                if (number == 1)
+                {
+                    Console.WriteLine($"{number}th odd number: {number}");
+                }
+                else
+                {
+                    Console.WriteLine($"{number}th odd number: {number + 2}");
+                }
+            }
         }
     }
 }
