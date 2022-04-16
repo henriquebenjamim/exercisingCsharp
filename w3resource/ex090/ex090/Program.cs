e88cf9aae876c6969e9d840664bccd2f96b361a1
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 16:40:49 -0300

    ex090 basic category resolved

--- w3resource/ex090/ex090/Program.cs
+++ w3resource/ex090/ex090/Program.cs
@@ -16,6 +16,38 @@ namespace ex090
             //Number of ones and zeros in the binary representation of the said number:
             //Number of ones: 5
             //Number of zeros: 6
+
+            Console.WriteLine(countingNumbersBinary(12));
+            Console.WriteLine(countingNumbersBinary(1234));
+        }
+
+        public static string countingNumbersBinary(int number)
+        {
+            string binary = Convert.ToString(number, 2);
+            // this base can be 2, 4, 8, 16
+
+            int countingNumberOne = 0;
+            int countingNumberZero = 0;
+
+            foreach(char c in binary)
+            {
+                if(c == '1' || c == '0')
+                {
+                    if(c == '1')
+                    {
+                        countingNumberOne += 1;
+                    }
+                    else
+                    {
+                        countingNumberZero += 1;
+                    }
+                }
+            }
+
+            return $"Original number: {number}\n" +
+                $"Number of ones and zeros in the binary representation of the said number:\n" +
+                $"Number of ones: {countingNumberOne}\n" +
+                $"Number of zeros: {countingNumberZero}\n";
         }
     }
 }
