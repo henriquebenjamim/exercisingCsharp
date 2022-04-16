257af0aa84cd2ad611b8b1d7297887077ced9e2c
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 15:46:07 -0300

    ex084 basic category resolved

--- w3resource/ex084/ex084/Program.cs
+++ w3resource/ex084/ex084/Program.cs
@@ -14,6 +14,18 @@ namespace ex084
             //Orginal string: JavaScript
             //Indices of all lower case letters of the said string:
             //1 2 3 5 6 7 8 9
+
+            Console.WriteLine("Give me a string: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Indices of all lower case letters of the said string: ");
+            foreach(char value in str)
+            {   
+                
+                if(Char.IsLower(value))
+                {
+                    Console.Write(str.IndexOf(value)+ " ");
+                }
+            }
         }
     }
 }
