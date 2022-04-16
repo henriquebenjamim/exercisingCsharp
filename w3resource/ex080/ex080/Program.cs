69efe28d7becf2ccd19e5d10b6b2b3b9b89de7f9
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 11:36:30 -0300

    ex080 basic category resolved

--- w3resource/ex080/ex080/Program.cs
+++ w3resource/ex080/ex080/Program.cs
@@ -14,12 +14,31 @@ namespace ex080
             //Value->False :: Type->System.Boolean
             //Value-> 4 / 15 / 2021 10:37:47 AM::Type->System.DateTime
             //Value-> 112.22 :: Type->System.Double
+
             //Printing array elements and their types:
             //Value-> 25 :: Type->System.String
             //Value->Anna :: Type->System.String
             //Value->False :: Type->System.String
             //Value-> 4 / 15 / 2021 10:37:47 AM::Type->System.String
             //Value-> 112.22 :: Type->System.String
+
+            object[] requiredArray = new object[5];
+            requiredArray[0] = 2016;
+            requiredArray[1] = System.DateTime.Now;
+            requiredArray[2] = "CodewithKasam";
+            requiredArray[3] = true;
+            requiredArray[4] = 4.6;
+            foreach(object value in requiredArray)
+            {
+                Console.WriteLine($"Value-> {value} :: Type-> {value.GetType()}");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Printing array elements and their types: ");
+            foreach (object value2 in requiredArray)
+            {
+                Console.WriteLine($"Value-> {value2} :: Type-> {value2.ToString().GetType()}");
+            }
+
         }
     }
 }
