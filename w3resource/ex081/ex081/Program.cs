5f53724a8ec0924f85f57279081cb1a83c0c5453
henriquebenjamim <henriiqqq@gmail.com>
2022-04-16 11:40:09 -0300

    Add ex082 basic category

--- w3resource/ex081/ex081/Program.cs
+++ w3resource/ex081/ex081/Program.cs
@@ -6,7 +6,10 @@ namespace ex081
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //81.Write a C# Sharp program to swap a two digit given number and check whether the given number is greater than its swap value.
+            //Sample Output:
+            //Input an integer value:
+            //Check whether the said value is greater than its swap value: True
         }
     }
 }
---
+++ w3resource/ex082/ex082/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex082
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //82.Write a C# Sharp program to remove all characters which are non-letters from a given string.
+            //From Wikipedia,
+            //A letter is a segmental symbol of a phonemic writing system. The inventory of all letters forms the alphabet.
+            //Letters broadly correspond to phonemes in the spoken form of the language, although there is rarely a consistent, exact correspondence between letters and phonemes
+            //Sample Output:
+            //Orginal string: Py @th12on
+            //Remove all characters from the said string which are non-letters: Python
+            //Orginal string: Python 3.0
+            //Remove all characters from the said string which are non-letters: Python
+            //Orginal string: 2 ^ sdfds * ^*^jlljdslfnoswje34u230sdfds984
+            //Remove all characters from the said string which are non-letters: sdfdsjlljdslfnoswjeusdfds
+        }
+    }
+}
