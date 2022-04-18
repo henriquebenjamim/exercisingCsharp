3a5f9ad40243a4e4cfa11a5e51081436bd7a37ae
henriquebenjamim <henriiqqq@gmail.com>
2022-04-17 21:10:28 -0300

    ex006 basic category resolved

---
+++ w3resource-basicAlgorithm/ex006/ex006/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ex006
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //6.Write a C# Sharp program to remove the character in a given position of a given string. The given position will be in the range 0.. string length -1 inclusive. Go to the editor
+
+            //Sample Input:
+            //"Python", 1
+            //"Python", o
+            //"Python", 4
+            //Expected Output:
+            //Pthon
+            //ython
+            //Pythn
+
+            Console.Write("Give me a string: ");
+            string name = Console.ReadLine();
+            Console.Write("Give me a position: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= name.Length - 1; i++)
+            {
+                if (i != number)
+                {
+                    Console.Write(name[i]);
+                }
+            }
+
+        }
+
+    }
+}
