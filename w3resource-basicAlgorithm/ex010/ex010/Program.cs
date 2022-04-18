eb86224d419cb19fb3982b690fdcc261ed1e9a9b
henriquebenjamim <henriiqqq@gmail.com>
2022-04-18 08:35:33 -0300

    ex010 basic algorithm resolved

---
+++ w3resource-basicAlgorithm/ex010/ex010/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex010
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //10.Write a C# Sharp program to check if a given positive number is a multiple of 3 or a multiple of 7. Go to the editor
+
+            //Sample Input:
+            //3
+            //14
+            //12
+            //37
+            //Expected Output:
+            //True
+            //True
+            //True
+            //False
+
+            Console.WriteLine(checkDivisors(3));
+            Console.WriteLine(checkDivisors(14));
+            Console.WriteLine(checkDivisors(12));
+            Console.WriteLine(checkDivisors(37));
+
+        }
+
+        public static bool checkDivisors(int number)
+        {
+            return number % 3 == 0 || number % 7 == 0 ? true : false;
+        }
+    }
+}
