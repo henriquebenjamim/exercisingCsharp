7360e600d8172f1943fd7e74277e125d1d493cf9
henriquebenjamim <henriiqqq@gmail.com>
2022-04-18 10:49:18 -0300

    ex019 basic algorithm resolved

---
+++ w3resource-basicAlgorithm/ex019/ex019/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ex019
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //19.Write a C# Sharp program to check which number nearest to the value 100 among two given integers. Return 0 if the two numbers are equal. Go to the editor
+
+            //Sample Input:
+            //78, 95
+            //95, 95
+            //99, 70
+            //Expected Output:
+            //95
+            //0
+            //99
+
+            Console.WriteLine(nearestNumber(78, 95));
+            Console.WriteLine(nearestNumber(95, 95));
+            Console.WriteLine(nearestNumber(99, 70));
+        }
+
+        public static int nearestNumber(int numberOne, int numberTwo)
+        {
+            int spreadOne = 100 - numberOne;
+            int spreadTwo = 100 - numberTwo;
+            return spreadOne != spreadTwo ? spreadOne > spreadTwo ? numberTwo : numberOne : 0 ;
+        }
+    }
+}
