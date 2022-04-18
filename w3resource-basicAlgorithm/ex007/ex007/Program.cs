36043753f024105a26a046edcbc7541431c64b25
henriquebenjamim <henriiqqq@gmail.com>
2022-04-17 21:39:44 -0300

    ex007 basic category resolved

---
+++ w3resource-basicAlgorithm/ex007/ex007/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ex007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //7.Write a C# Sharp program to exchange the first and last characters in a given string and return the new string. Go to the editor
+
+            //Sample Input:
+            //"abcd"
+            //"a"
+            //"xy"
+            //Expected Output:
+
+            //dbca
+            //a
+            //yx
+
+            Console.WriteLine(reversingCharacters("abcd"));
+            Console.WriteLine(reversingCharacters("a"));
+            Console.WriteLine("xy");
+        }
+
+        public static string reversingCharacters(string str)
+        {
+            return str.Length > 1 ? str : str.Substring(1, str.Length - 1) + str.Substring(0, 1);
+        }
+    }
+}
