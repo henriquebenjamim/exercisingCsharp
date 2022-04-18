7634ac6beacfdc3c4e185e0469562652bf9163e9
henriquebenjamim <henriiqqq@gmail.com>
2022-04-18 08:33:25 -0300

    ex009 basic algorithm resolved

---
+++ w3resource-basicAlgorithm/ex008/ex008/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex008
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //8.Write a C# Sharp program to create a new string which is 4 copies of the 2 front characters of a given string. If the given string length is less than 2 return the original string. Go to the editor
+
+            //Sample Input:
+            //"C Sharp"
+            //"JS"
+            //"a"
+            //Expected Output:
+            //C C C C
+            //JSJSJSJS
+            //a
+
+            string name = "henrique";
+            Console.WriteLine(name.Substring(0, 2) + name.Substring(0, 2));
+
+        }
+
+        public static string fourCopiesString(string str)
+        {
+            return str.Length == 1 ? str : str.SubString(0, 2) + str.SubString(0, 2) + str.SubString(0, 2) + str.SubString(0, 2)
+        }
+    }
+}
---
+++ w3resource-basicAlgorithm/ex009/ex009/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ex009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //9.Write a C# Sharp program to create a new string with the last char added at the front and back of a given string of length 1 or more. Go to the editor
+
+            //Sample Input:
+            //"Red"
+            //"Green"
+            //"1"
+
+            //Expected Output:
+            //dRedd
+            //nGreenn
+            //111
+
+            Console.WriteLine(addingCharacters("Red"));
+            Console.WriteLine(addingCharacters("Green"));
+            Console.WriteLine(addingCharacters("1"));
+
+        }
+
+        public static string addingCharacters(string str)
+        {
+            return str.Length >= 1 ? str[str.Length - 1] + str + str[str.Length - 1]: str;
+        }
+    }
+}
