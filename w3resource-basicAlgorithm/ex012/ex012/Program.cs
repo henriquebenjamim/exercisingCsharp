e0873abdaaebc8d99c1e76a7557a26736e607404
henriquebenjamim <henriiqqq@gmail.com>
2022-04-18 08:43:24 -0300

    ex012 basic algorithm resolved

---
+++ w3resource-basicAlgorithm/ex012/ex012/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex012
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //12.Write a C# Sharp program to check if a given string starts with 'C#' or not. Go to the editor
+
+            //Sample Input:
+            //"C# Sharp"
+            //"C#"
+            //"C++"
+            //Expected Output:
+            //True
+            //True
+            //False
+            Console.WriteLine(checkCsharpStart("C# Sharp"));
+            Console.WriteLine(checkCsharpStart("C#"));
+            Console.WriteLine(checkCsharpStart("C++"));
+            Console.WriteLine(checkCsharpStart("c# it's the language that i'm using"));
+        }
+
+        public static bool checkCsharpStart(string str)
+        {
+            return str.Length >= 2 ? (str[0] == 'C' || str[0] == 'c') && str[1] == '#' ? true : false : false; 
+        }
+    }
+}
