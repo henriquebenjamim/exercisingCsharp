b32b7d409e6ae2ed603db79f50fd1fbdf697d255
henriquebenjamim <henriiqqq@gmail.com>
2022-04-18 08:57:06 -0300

    ex014 basic algorithm resolved

---
+++ w3resource-basicAlgorithm/ex014/ex014/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //14.Write a C# Sharp program to check two given integers whether either of them is in the range 100..200 inclusive.
+
+            //Sample Input:
+            //100, 199
+            //250, 300
+            //105, 190
+            //Expected Output:
+            //True
+            //False
+            //True
+
+            Console.WriteLine(checkRangeNumbers(100, 199));
+            Console.WriteLine(checkRangeNumbers(250, 300));
+            Console.WriteLine(checkRangeNumbers(105, 190));
+        }
+
+        public static bool checkRangeNumbers(int numberOne, int numberTwo)
+        {
+            return (numberOne >= 100 && numberTwo <= 200) || (numberTwo >= 100 && numberOne <= 200) ? true : false;
+        }
+    }
+}
