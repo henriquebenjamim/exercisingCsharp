43eb703937d8a71b672b48ed03cf22d91b9e7bec
henriquebenjamim <henriiqqq@gmail.com>
2022-04-19 20:41:10 -0300

    ex021 basic algorithm added

---
+++ w3resource-basicAlgorithm/ex020/ex020/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ex020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //20.Write a C# Sharp program to check whether two given integers are in the range 40..50 inclusive, or they are both in the range 50..60 inclusive. Go to the editor
+
+            //Sample Input:
+            //78, 95
+            //25, 35
+            //40, 50
+            //55, 60
+            //Expected Output:
+            //False
+            //False
+            //True
+            //True
+        }
+
+        public static bool checkingRanges(int numberOne, int numberTwo)
+        {
+
+        }
+    }
+}
---
+++ w3resource-basicAlgorithm/ex021/ex021/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex021
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //21.Write a C# Sharp program to find the larger value from two positive integer values that is in the range 20..30 inclusive, or return 0 if neither is in that range. Go to the editor
+
+            //Sample Input:
+            //78, 95
+            //20, 30
+            //21, 25
+            //28, 28
+            //Expected Output:
+            //0
+            //30
+            //25
+            //28
+        }
+    }
+}
