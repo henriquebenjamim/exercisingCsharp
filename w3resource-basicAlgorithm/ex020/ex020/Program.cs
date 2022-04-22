b0c99ec7a6dc1eb0628e7795ffd45c8fd252ab05
henriquebenjamim <henriiqqq@gmail.com>
2022-04-21 23:06:35 -0300

    ex020 basic algorithm resolved

--- w3resource-basicAlgorithm/ex020/ex020/Program.cs
+++ w3resource-basicAlgorithm/ex020/ex020/Program.cs
@@ -18,11 +18,16 @@ namespace ex020
             //False
             //True
             //True
+
+            Console.WriteLine(checkingRanges(78, 95));
+            Console.WriteLine(checkingRanges(25, 35));
+            Console.WriteLine(checkingRanges(40, 50));
+            Console.WriteLine(checkingRanges(55, 60));
         }
 
         public static bool checkingRanges(int numberOne, int numberTwo)
         {
-
+            return ((numberOne >= 40 && numberOne <= 50) || (numberTwo >= 40 && numberTwo <= 50)) || ((numberOne >= 50 && numberOne <= 60) || (numberTwo >= 50 && numberTwo <= 60)) ? true : false;
         }
     }
 }
