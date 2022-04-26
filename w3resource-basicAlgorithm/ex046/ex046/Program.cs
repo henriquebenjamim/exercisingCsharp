d85c56a53267cacdb165f446a84ff13aa71b056f
henriquebenjamim <henriiqqq@gmail.com>
2022-04-25 22:04:17 -0300

    ex046 basic algorithm resolved

--- w3resource-basicAlgorithm/ex046/ex046/Program.cs
+++ w3resource-basicAlgorithm/ex046/ex046/Program.cs
@@ -6,7 +6,9 @@ namespace ex046
     {
         static void Main(string[] args)
         {
-            //46.Write a C# Sharp program to check whether a given string starts with "F" or ends with "B". If the string starts with "F" return "Fizz" and return "Buzz" if it ends with "B" If the string starts with "F" and ends with "B" return "FizzBuzz". In other cases return the original string. Go to the editor
+            //46.Write a C# Sharp program to check whether a given string starts with "F" or ends with "B". If the string starts with "F" return "Fizz"
+            //and return "Buzz" if it ends with "B" If the string starts with "F" and ends with "B" return "FizzBuzz".
+            //In other cases return the original string.
 
             //Sample Input:
             //"FizzBuzz"
@@ -18,6 +20,41 @@ namespace ex046
             //Fizz
             //Buzz
             //Fizz
+
+            Console.WriteLine(fizzBuzz("FizzBuzz"));
+            Console.WriteLine(fizzBuzz("Fizz"));
+            Console.WriteLine(fizzBuzz("Buzz"));
+            Console.WriteLine(fizzBuzz("Founder"));
+            Console.WriteLine(fizzBuzz("FizzBubbleB"));
         }
+
+        public static string fizzBuzz(string str)
+        {
+            string answerStr = string.Empty;
+            bool firstCondition = str[0] == 'F';
+            bool secondCondition = str[str.Length - 1] == 'B';
+            if(firstCondition && secondCondition)
+            {
+                answerStr += "FizzBuzz";
+            }
+            else
+            {
+                if(firstCondition)
+                {
+                    answerStr += "Fizz";
+                }
+                else if(secondCondition)
+                {
+                    answerStr += "Buzz";
+                }
+                else
+                {
+                    answerStr += str;
+                }
+            }
+
+            return answerStr;
+        }
+
     }
 }
