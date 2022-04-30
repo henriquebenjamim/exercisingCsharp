8e8f283f275b8a034458c87f0f15424d9dede9ec
henriquebenjamim <henriiqqq@gmail.com>
2022-04-29 22:25:22 -0300

    ex055 basic algorithm resolved

--- w3resource-basicAlgorithm/ex055/ex055/Program.cs
+++ w3resource-basicAlgorithm/ex055/ex055/Program.cs
@@ -18,6 +18,18 @@ namespace ex055
             //23
             //12
             //18
+
+            Console.WriteLine(tripleSum(4, 5, 7));
+            Console.WriteLine(tripleSum(7, 4, 12));
+            Console.WriteLine(tripleSum(10, 10, 12));
+            Console.WriteLine(tripleSum(12, 12, 18));
+        }
+
+        public static int tripleSum(int numberOne, int numberTwo, int numberThree)
+        {
+            bool tripleCondition = (numberOne == numberTwo) || (numberOne == numberThree) || (numberThree == numberTwo);
+            int sum = numberOne + numberTwo + numberThree;
+            return tripleCondition ? numberThree : sum;
         }
     }
 }
