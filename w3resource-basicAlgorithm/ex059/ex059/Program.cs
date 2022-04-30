a0089927c0ff1c0857050c4d39dbce7f2fa8f709
henriquebenjamim <henriiqqq@gmail.com>
2022-04-29 23:19:32 -0300

    ex059 basic algorithm resolved

--- w3resource-basicAlgorithm/ex059/ex059/Program.cs
+++ w3resource-basicAlgorithm/ex059/ex059/Program.cs
@@ -17,6 +17,15 @@ namespace ex059
             //True
             //False
             //True
+
+            Console.WriteLine(sameDifference(4, 5, 6));
+            Console.WriteLine(sameDifference(7, 12, 13));
+            Console.WriteLine(sameDifference(-1, 0, 1));
+        }
+
+        public static bool sameDifference(int numberOne, int numberTwo, int numberThree)
+        {
+            return numberThree - numberTwo == numberTwo - numberOne ? true : false;
         }
     }
 }
