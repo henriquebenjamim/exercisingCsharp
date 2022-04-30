b78d825fbb380cd261d467092c53348f550c59fd
henriquebenjamim <henriiqqq@gmail.com>
2022-04-29 23:11:21 -0300

    ex057 basic algorithm resolved

--- w3resource-basicAlgorithm/ex057/ex057/Program.cs
+++ w3resource-basicAlgorithm/ex057/ex057/Program.cs
@@ -18,6 +18,42 @@ namespace ex057
             //11
             //13
             //17
+
+            Console.WriteLine(tripleSumRangeConditions(4, 5, 7));
+            Console.WriteLine(tripleSumRangeConditions(7, 4, 12));
+            Console.WriteLine(tripleSumRangeConditions(10, 13, 12));
+            Console.WriteLine(tripleSumRangeConditions(17, 12, 18));
+        }
+
+        public static int tripleSumRangeConditions(int numberOne, int numberTwo, int numberThree)
+        {
+            bool firstCondition = numberOne >= 10 && numberOne <= 20 && numberOne != 13 && numberOne != 17;
+            bool secondCondition = numberTwo >= 10 && numberTwo <= 20 && numberTwo != 13 && numberTwo != 17;
+            bool thirthCondition = numberThree >= 10 && numberThree <= 20 && numberThree != 13 && numberThree != 17;
+
+            int sum = 0;
+
+            if (!firstCondition && !secondCondition && !thirthCondition)
+            {
+                sum = numberOne + numberTwo + numberThree;
+            }
+            else
+            {
+                if (!firstCondition)
+                {
+                    sum += numberOne;
+                }
+                if(!secondCondition)
+                {
+                    sum += numberTwo;
+                }
+                if(!thirthCondition)
+                {
+                    sum += numberThree;
+                }
+            }
+
+            return sum;
         }
     }
 }
