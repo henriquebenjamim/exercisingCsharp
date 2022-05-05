8791b9afac195798bdbd6bff91d562784bd88394
henriquebenjamim <henriiqqq@gmail.com>
2022-05-05 15:32:23 -0300

    ex082 basic algorithm resolved

--- w3resource-basicAlgorithm/ex082/ex082/Program.cs
+++ w3resource-basicAlgorithm/ex082/ex082/Program.cs
@@ -6,7 +6,8 @@ namespace ex082
     {
         static void Main(string[] args)
         {
-            //82.Write a C# Sharp program to combine two given strings. If the given strings have different length remove the characters from the longer string.
+            //82.Write a C# Sharp program to combine two given strings.
+            //If the given strings have different length remove the characters from the longer string.
 
             //Sample Input:
             //"abc", "abcd"
@@ -16,6 +17,18 @@ namespace ex082
             //abcbcd
             //PythonPython
             //JSon
+
+            Console.WriteLine(combineStr("abc", "abcd"));
+            Console.WriteLine(combineStr("Python", "Python"));
+            Console.WriteLine(combineStr("JS", "Python"));
+            
+        }
+
+        public static string combineStr(string str1, string str2)
+        {
+            return str1.Length < str2.Length ? $"{str1}{str2.Substring(str2.Length - str1.Length, str1.Length)}" : 
+                str1.Length > str2.Length ? $"{str1}{str2.Substring(str2.Length - str1.Length, str1.Length)}" :
+                str1.Length == str2.Length ? str1 + str2: "";
         }
     }
 }
