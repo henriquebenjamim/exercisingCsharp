d132b3ec9924232ccbecba68ab7e5f6345661b8e
henriquebenjamim <henriiqqq@gmail.com>
2022-05-05 15:36:33 -0300

    ex083 basic algorithm resolved

--- w3resource-basicAlgorithm/ex083/ex083/Program.cs
+++ w3resource-basicAlgorithm/ex083/ex083/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex083
 {
@@ -6,7 +7,8 @@ namespace ex083
     {
         static void Main(string[] args)
         {
-            //83.Write a C# Sharp program to create a new string using 3 copies of the first 2 characters of a given string. If the length of the given string is less than 2 use the whole string.
+            //83.Write a C# Sharp program to create a new string using 3 copies of the first 2 characters of a given string.
+            //If the length of the given string is less than 2 use the whole string.
 
             //Sample Input:
             //"abc"
@@ -16,6 +18,16 @@ namespace ex083
             //ababab
             //PyPyPy
             //JJJ
+
+            Console.WriteLine(threeCopies("abc"));
+            Console.WriteLine(threeCopies("Python"));
+            Console.WriteLine(threeCopies("J"));
+        }
+
+        public static string threeCopies(string str)
+        {
+
+            return str.Length > 2 ? string.Concat(Enumerable.Repeat(str.Substring(0, 2), 3)) : string.Concat(Enumerable.Repeat(str, 3));
         }
     }
 }
