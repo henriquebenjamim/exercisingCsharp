e3e3576cfa664bcad3712512c095a6adcc3ee5f5
henriquebenjamim <henriiqqq@gmail.com>
2022-05-07 18:58:20 -0300

    ex090 basic algorithm resolved

--- w3resource-basicAlgorithm/ex090/ex090/Program.cs
+++ w3resource-basicAlgorithm/ex090/ex090/Program.cs
@@ -6,7 +6,8 @@ namespace ex090
     {
         static void Main(string[] args)
         {
-            //90.Write a C# Sharp program to check two given arrays of integers of length 1 or more and return true if they have the same first element or they have the same last element.
+            //90.Write a C# Sharp program to check two given arrays of integers of length 1 or more and return true if they have the same first element
+            //or they have the same last element.
 
             //Sample Input:
             //{ 10, 20, 40, 50 }, { 10, 20, 40, 50 }
@@ -16,6 +17,17 @@ namespace ex090
             //True
             //True
             //False
+
+            Console.WriteLine(firstLastElementsEquals(new[] {10, 20, 40, 50}, new[] {10, 20, 40, 50}));
+            Console.WriteLine(firstLastElementsEquals(new[] { 10, 20, 40, 50}, new[] { 10, 20, 40, 5}));
+            Console.WriteLine(firstLastElementsEquals(new[] { 10, 20, 40, 50}, new[] { 1, 20, 40, 5}));
+        }
+
+        public static bool firstLastElementsEquals(int[] firstArr, int[] secondArr)
+        {
+            return firstArr.Length > 1 && secondArr.Length > 1 ? 
+                firstArr[0] == secondArr[0] || firstArr[firstArr.Length - 1] == secondArr[secondArr.Length - 1] ?
+                true : false : false;
         }
     }
 }
