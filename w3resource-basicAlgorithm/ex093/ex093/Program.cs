7e416c2e14fefc255c243daef2c54666e8d9b612
henriquebenjamim <henriiqqq@gmail.com>
2022-05-07 19:26:52 -0300

    ex093 basic algorithm resolved

--- w3resource-basicAlgorithm/ex093/ex093/Program.cs
+++ w3resource-basicAlgorithm/ex093/ex093/Program.cs
@@ -12,6 +12,15 @@ namespace ex093
             //{ 10, 20, -30, -40, 50 }
             //Expected Output:
             //Reverse array: 50 - 40 - 30 20 10
+
+            int[] numberArr = { 10, 20, -30, -40, 50 };
+            Console.WriteLine("Reverse array: ");
+            
+            for (int i = numberArr.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{numberArr[i]} ");
+            }
         }
+
     }
 }
