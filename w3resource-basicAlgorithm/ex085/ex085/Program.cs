15b330aab25bdc86f13a6b8d6413100a09941976
henriquebenjamim <henriiqqq@gmail.com>
2022-05-06 21:12:55 -0300

    ex085 basic algorithm resolved

--- w3resource-basicAlgorithm/ex085/ex085/Program.cs
+++ w3resource-basicAlgorithm/ex085/ex085/Program.cs
@@ -6,7 +6,8 @@ namespace ex085
     {
         static void Main(string[] args)
         {
-            //85.Write a C# Sharp program to create a new string from a given string without the first two characters. Keep the first character if it is "p" and keep the second character if it is "y".
+            //85.Write a C# Sharp program to create a new string from a given string without the first two characters.
+            //Keep the first character if it is "p" and keep the second character if it is "y".
 
             //Sample Input:
             //"abcab"
@@ -18,6 +19,19 @@ namespace ex085
             //python
             //pess
             //ython
+
+            Console.WriteLine(firstPSecondY("abcab"));
+            Console.WriteLine(firstPSecondY("python"));
+            Console.WriteLine(firstPSecondY("press"));
+            Console.WriteLine(firstPSecondY("jython"));
+        }
+
+        public static string firstPSecondY(string str)
+        {
+            return str.Length > 2 && (str.Substring(0, 1) == "p" && str.Substring(1, 1) == "y") ? str: 
+                str.Substring(0, 1) == "p" ? str.Substring(0, 1) + str.Substring(2) :
+                str.Substring(1, 1) == "y" ? str.Substring(1) : str.Substring(2);
+
         }
     }
 }
