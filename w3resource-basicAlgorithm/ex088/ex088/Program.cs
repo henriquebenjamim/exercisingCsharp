a5f9b93ff3e895ec495cbc692f38ecc6ab8482a5
henriquebenjamim <henriiqqq@gmail.com>
2022-05-07 18:39:41 -0300

    ex089 basic algorithm resolved

--- w3resource-basicAlgorithm/ex088/ex088/Program.cs
+++ w3resource-basicAlgorithm/ex088/ex088/Program.cs
@@ -6,7 +6,8 @@ namespace ex088
     {
         static void Main(string[] args)
         {
-            //88.Write a C# Sharp program to check a given array of integers of length 1 or more and return true if 10 appears as either first or last element in the given array. Go to the editor
+            //88.Write a C# Sharp program to check a given array of integers of length 1 or more and return true if
+            //10 appears as either first or last element in the given array.
 
             //Sample Input:
             //{ 10, 20, 40, 50 }
@@ -18,6 +19,16 @@ namespace ex088
             //True
             //True
             //False
+
+            Console.WriteLine(tenInArray(new[] { 10, 20, 40, 50}));
+            Console.WriteLine(tenInArray(new[] { 5, 20, 40, 10}));
+            Console.WriteLine(tenInArray(new[] { 10, 20, 40, 10}));
+            Console.WriteLine(tenInArray(new[] { 12, 24, 35, 55}));
+        }
+
+        public static bool tenInArray(int[] numberArr)
+        {
+            return numberArr.Length > 1 ? numberArr[0] == 10 || numberArr[numberArr.Length - 1] == 10 ? true : false : false;
         }
     }
 }
--- w3resource-basicAlgorithm/ex089/ex089/Program.cs
+++ w3resource-basicAlgorithm/ex089/ex089/Program.cs
@@ -6,7 +6,8 @@ namespace ex089
     {
         static void Main(string[] args)
         {
-            //89.Write a C# Sharp program to check a given array of integers of length 1 or more and return true if the first element and the last element are equal in the given array.
+            //89.Write a C# Sharp program to check a given array of integers of length 1 or more and return true if the first element and
+            //the last element are equal in the given array.
 
             //Sample Input:
             //{ 10, 20, 40, 50 }
@@ -16,6 +17,15 @@ namespace ex089
             //False
             //True
             //False
+
+            Console.WriteLine(firstLastEqualsArr(new[] { 10, 20, 40, 50}));
+            Console.WriteLine(firstLastEqualsArr(new[] { 10, 20, 40, 10}));
+            Console.WriteLine(firstLastEqualsArr(new[] { 12, 24, 35, 55}));
+        }
+
+        public static bool firstLastEqualsArr(int[] numberArr)
+        {
+            return numberArr.Length > 1 ? numberArr[0] == numberArr[numberArr.Length - 1] ? true : false : false;
         }
     }
 }
