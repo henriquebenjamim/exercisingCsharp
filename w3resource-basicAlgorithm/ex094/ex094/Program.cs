39f24a1bb853618a40c07dacd8469c87e5e62a3c
henriquebenjamim <henriiqqq@gmail.com>
2022-05-07 19:29:09 -0300

    ex094 basic algorithm resolved

--- w3resource-basicAlgorithm/ex094/ex094/Program.cs
+++ w3resource-basicAlgorithm/ex094/ex094/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex094
 {
@@ -12,6 +13,13 @@ namespace ex094
             //{ 10, 20, -30, -40 }
             //Expected Output:
             //New array: 20 20 20 20
+
+            Console.WriteLine(maximumRepeated(new[] { 10, 20, -30, -40}));
+        }
+
+        public static string maximumRepeated(int[] numbersArr)
+        {
+            return $"New array: {numbersArr.Max()} {numbersArr.Max()} {numbersArr.Max()} {numbersArr.Max()}";
         }
     }
 }
