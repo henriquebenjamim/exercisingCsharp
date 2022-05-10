48f627c3ec1ad567a2190ec4d305de4773245f5f
henriquebenjamim <henriiqqq@gmail.com>
2022-05-10 20:50:20 -0300

    ex099 basic algorithm resolved

--- w3resource-basicAlgorithm/ex099/ex099/Program.cs
+++ w3resource-basicAlgorithm/ex099/ex099/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex099
 {
@@ -7,12 +8,21 @@ namespace ex099
         static void Main(string[] args)
         {
             //99.Write a C# Sharp program to create a new array of integers and length 1 or more.
-            //The length of the new array will be double length of the given array and all the elements are 1 except the first element which is equal to the given array.
+            //The length of the new array will be double length of the given array and all the elements
+            //are 1 except the first element which is equal to the given array.
 
             //Sample Input:
             //{ 10, 20, -30, -40, 30 }
             //Expected Output :
             //New array: 10 0 0 0 0 0 0 0 0 0
+
+            Console.WriteLine(onlyFirstNormal(new[] { 10, 20, -30, -40, 30}));
+        }
+
+        public static string onlyFirstNormal(int[] number)
+        {
+            int rep = number.Length * 2 - 1;
+            return number.Length >= 1 ? $"New array: {number[0]} {string.Concat(Enumerable.Repeat("0 ", rep))}" : $"We don't have any value.";
         }
     }
 }
