0f59191f38c4b0ae2b6745b517c1e25cc5f6d774
henriquebenjamim <henriiqqq@gmail.com>
2022-05-11 18:02:10 -0300

    ex0102 basic algorithm resolved

--- w3resource-basicAlgorithm/ex101/ex101/Program.cs
+++ w3resource-basicAlgorithm/ex101/ex101/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ex101
 {
@@ -6,13 +7,34 @@ namespace ex101
     {
         static void Main(string[] args)
         {
-            //101.Write a C# Sharp program to check a given array of integers, length 3 and create a new array. If there is a 5 in the given array immediately followed by a 7 then set 7 to 1.
+            //101.Write a C# Sharp program to check a given array of integers, length 3 and create a new array.
+            //If there is a 5 in the given array immediately followed by a 7 then set 7 to 1.
 
             //Sample Input:
             //{ 1, 5, 7 }
-            //            Expected Output :
+            //Expected Output :
             //Original array:  1, 5, 7
             //New array: 1 5 1
+
+            int[] arr = fiftySeven(new[] { 1, 5, 3 });
+            Console.WriteLine("New array: ");
+            foreach (int value in arr)
+            {
+                Console.Write($"{value} ");
+            }
+
+        }
+
+        public static int[] fiftySeven(int[] numbersArr)
+        {
+            for (int i = 0; i < numbersArr.Length - 1; i++)
+            {
+                if (numbersArr[i].Equals(5) && numbersArr[i + 1].Equals(7))
+                {
+                    numbersArr[i + 1] = 1;
+                }
+            }
+            return numbersArr;
         }
     }
 }
