10b6682250bc5695de7d1e01f2d9cb13c5cddc9d
henriquebenjamim <henriiqqq@gmail.com>
2022-05-12 17:45:57 -0300

    ex102 basic algorithm resolved

--- w3resource-basicAlgorithm/ex0102/ex0102/Program.cs
+++ w3resource-basicAlgorithm/ex0102/ex0102/Program.cs
@@ -14,6 +14,19 @@ namespace ex0102
             //Expected Output :
             //Original array:  1, 5, 7
             //Larger array: 10 20 30
+
+            int[] item = test(new[] { 10, 20, -30 }, new[] { 10, 20, 30 });
+            Console.Write("Larger array: ");
+            foreach (var i in item)
+            {
+                Console.Write(i.ToString() + " ");
+            }
         }
+
+        public static int[] test(int[] numbersArrOne, int[] numbersArrTwo)
+        {
+            return numbersArrOne[0] + numbersArrOne[1] + numbersArrOne[2] >= numbersArrTwo[0] + numbersArrTwo[1] + numbersArrTwo[2] ? numbersArrOne : numbersArrTwo;
+        }
+
     }
 }
