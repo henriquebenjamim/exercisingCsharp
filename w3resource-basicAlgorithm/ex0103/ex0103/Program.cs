cfe671594bd233bb6c1789a67a3a849f8b105229
henriquebenjamim <henriiqqq@gmail.com>
2022-05-13 18:11:01 -0300

    ex basic algorithm resolved

--- w3resource-basicAlgorithm/ex0103/ex0103/Program.cs
+++ w3resource-basicAlgorithm/ex0103/ex0103/Program.cs
@@ -13,6 +13,13 @@ namespace ex0103
             //Expected Output:
             //Original array: 1, 5, 7, 9, 11, 13
             //New array: 7 9
+
+            Console.WriteLine(numbersMiddleArr(new[] { 1, 5, 7, 9, 11, 13}));
+        }
+
+        public static string numbersMiddleArr(int[] numbersArr)
+        {
+            return numbersArr.Length % 2 != 0 ? $"I can't do this, lenght array is odd" : $"New array: {numbersArr[numbersArr.Length / 2 - 1]} {numbersArr[numbersArr.Length / 2]}";
         }
     }
 }
