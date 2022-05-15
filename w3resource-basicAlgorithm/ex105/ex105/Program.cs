f1a2a715d9c1dfa86c27e60595e263ef9b587739
henriquebenjamim <henriiqqq@gmail.com>
2022-05-15 20:54:17 -0300

    ex105 basic algorithm resolved

--- w3resource-basicAlgorithm/ex105/ex105/Program.cs
+++ w3resource-basicAlgorithm/ex105/ex105/Program.cs
@@ -13,6 +13,22 @@ namespace ex105
             //Expected Output :
             //Original array: 1, 5, 7, 9, 11, 13
             //After swapping first and last elements: 13 5 7 9 11 1
+
+            int[] originalNumbersArr = newArr(new[] { 1, 5, 7, 9, 11, 13 });
+            Console.Write("After swapping first n last elements: ");
+            foreach (int value in originalNumbersArr)
+            {
+                Console.Write($"{value} ");
+            }
+        }
+
+        public static int[] newArr(int[] numbersArr)
+        {
+            int substitute = numbersArr[0];
+            numbersArr[0] = numbersArr[numbersArr.Length - 1];
+            numbersArr[numbersArr.Length - 1] = substitute;
+
+            return numbersArr;
         }
     }
 }
