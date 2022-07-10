c96388e90e817f738c32c5155bc88f513c30cb77
henriquebenjamim <henriiqqq@gmail.com>
2022-07-09 22:26:21 -0300

    basic algorithm 108 resolved

--- w3resource-basicAlgorithm/ex108/ex108/Program.cs
+++ w3resource-basicAlgorithm/ex108/ex108/Program.cs
@@ -13,6 +13,44 @@ namespace ex108
             //{ 1, 5, 7, 9, 11, 13 }
             //Expected Output :
             //New array: 1 5
+
+            int[] exampleArray = gettingTwoFirstOnArray(new[] { 1, 2, 3, 4, 5, 6 });
+            int[] exampleArray2 = gettingTwoFirstOnArray(new[] { 1, 5, 7, 9, 11, 13 });
+            int[] exampleArray3 = gettingTwoFirstOnArray(new[] { 9 });
+
+            foreach (var example in exampleArray)
+            {
+                Console.Write($"{example.ToString()} ");
+            }
+            foreach (var example in exampleArray2)
+            {
+                Console.Write($"{example.ToString()} ");
+            }
+            foreach (var example in exampleArray3)
+            {
+                Console.Write($"{example.ToString()} ");
+            }
+
+        }
+
+        static int[] gettingTwoFirstOnArray(int[] numberArray)
+        {
+            int[] numbersToGet;
+
+            if (numberArray.Length >= 2)
+            {
+                numbersToGet = new int[] { numberArray[0], numberArray[1] };
+            }
+            else if(numberArray.Length > 0)
+            {
+                numbersToGet = new int[] { numberArray[0] };
+            }
+            else
+            {
+                numbersToGet = new int[0];
+            }
+
+            return numbersToGet;
         }
     }
 }
